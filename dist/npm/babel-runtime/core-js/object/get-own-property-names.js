b60a97a255f6cac11959ef62a4bccac96c84c3ce
Ouye Xie <o@shengupiao.com>
2016-11-11 14:34:00 +0800

    remove dist

--- dist/npm/babel-runtime/core-js/object/get-own-property-names.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-(function(module,require){var exports=module.exports={};
-module.exports = { "default": require('../../../core-js/library/fn/object/get-own-property-names.js'), __esModule: true };
-})(module,require);
--- src/pages/logs/logs.js
+++ src/pages/logs/logs.js
@@ -1,6 +1,6 @@
 import wx from 'labrador';
 import { formatTime } from '../../utils/util';
-//import loadViewer from '../../actions/viewer'
+import loadViewer from '../../actions/viewer'
 //import 'whatwg-fetch'
 
 export default class Logs extends wx.Component {
@@ -18,29 +18,35 @@ export default class Logs extends wx.Component {
 //        console.log('++++++', body)
 //      }))
 //    console.log('+++++++++++++')
-    console.log('+++++++++++++')
-    await wx.request({
-      url: 'http://127.0.0.1:3012/ping', //仅为示例，并非真实的接口地址
-//      data: {
-//        x: '' ,
-//        y: ''
+    
+    
+    
+//    console.log('+++++++++++++')
+//    await wx.request({
+//      url: 'http://127.0.0.1:3012/ping', //仅为示例，并非真实的接口地址
+////      data: {
+////        x: '' ,
+////        y: ''
+////      },
+////      header: {
+////        'Content-Type': 'application/json'
+////      },
+//      success: function(res) {
+//        console.log('s+',res.data)
 //      },
-//      header: {
-//        'Content-Type': 'application/json'
+//      fail: function(res) {
+//        console.log('f+',res.data)
 //      },
-      success: function(res) {
-        console.log('s+',res.data)
-      },
-      fail: function(res) {
-        console.log('f+',res.data)
-      },
-      complete: function(res) {
-        console.log('c+',res)
-      }
-    })
+//      complete: function(res) {
+//        console.log('c+',res)
+//      }
+//    })
+//    console.log('+++++++++++++')
+
+
     console.log('+++++++++++++')
-//    const viewer = await loadViewer();
-//    console.log('++++++++++++', viewer)
+    const viewer = await loadViewer();
+    console.log('++++++++++++', viewer)
     let res = await wx.getStorage({key: 'logs'});
     let logs = res.data || [];
     this.setData({
